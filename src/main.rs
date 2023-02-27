9de9a6d63bc9e2e32d6c2c497f73e19790fdc314
Andreas <aka@jma.dk>
2023-02-27 09:55:15 +0100

    create scss and event

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::process::{Command};
+use std::process::{Command, Stdio};
+use std::io::{BufRead, BufReader};
 use std::str;
 use serde::{Serialize, Deserialize};
 use serde_json::{Result};
@@ -14,7 +15,7 @@ struct Window {
     urgent: bool,
 }
 
-fn main() -> Result<()> {
+fn workspace() -> Result<()> {
     let output = Command::new("i3-msg").args(["-t", "get_workspaces"]).output().expect("failed to execute process");
 
     let s = match str::from_utf8(&output.stdout) {
@@ -24,10 +25,37 @@ fn main() -> Result<()> {
 
     let windows: Vec<Window> = serde_json::from_str(s)?;
 
+    let mut works_vec = Vec::new();
     for window in windows {
-        if window.focused {
-            println!("focused_window: {}", window.name);
+        if !window.visible {
+            works_vec.push(format!("(button :onclick \"i3-msg 'workspace {}'\" :class \"works\" \"{}\")", window.name, "unoccupied"));
         }
+        if window.visible {
+            works_vec.push(format!("(button :onclick \"i3-msg 'workspace {}'\" :class \"works\" \"{}\")", window.name, "occupied"));
+        }
+        if window.focused {
+            works_vec.push(format!("(button :onclick \"i3-msg 'workspace {}'\" :class \"works\" \"{}\")", window.name,"focused"));
+        }
+    }
+
+    let works_str = works_vec.join("\n\t\t\t");
+    let rust_str = format!("(box :class \"works\" :orientation \"h\" :halign \"center\" :space-evenly \"false\" :spacing \"12\"\n\t{}\n)", works_str);
+
+    println!("{}", rust_str);
+
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let i3_msg_process = Command::new("i3-msg")
+    .args(["-t", "subscribe", "-m", r#"["window", "workspace"]"#])
+    .stdout(Stdio::piped())
+    .spawn()?;
+
+    let reader = BufReader::new(i3_msg_process.stdout.expect("failed to get stdout"));
+
+    for _line in reader.lines() {
+        workspace().map_err(|err| println!("{:?}", err)).ok();
     }
 
     Ok(())
